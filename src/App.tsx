e4787948365cd7f32df20bcda9b9e7af96ac15ce
gygabor <gyorgyg@gmail.com>
2023-12-20 09:52:38 +0100

    feat(App.tsx): add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,25 @@
-const App = () => (
-  <div>
-    <h1>Hello, World!</h1>
-  </div>
-)
+import { FC } from 'react'
+import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import routes from './constants/routes'
+import Cities from './pages/Cities'
+import Weather from './pages/Weather'
+import Home from './pages/Home'
+
+const router = createBrowserRouter([
+  {
+    path: routes.HOME,
+    element: <Home />,
+  },
+  {
+    path: routes.CITIES,
+    element: <Cities />,
+  },
+  {
+    path: routes.WEATHER,
+    element: <Weather />,
+  },
+])
+
+const App: FC = () => <RouterProvider router={router} />
 
 export default App
