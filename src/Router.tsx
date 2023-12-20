b3c402d5f9d4ddc9252fdf474bc649274c533f88
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-20 23:23:38 +0100

    feat(Router): create layout, nested routes

--- src/Router.tsx
+++ src/Router.tsx
@@ -4,19 +4,26 @@ import routes from '@src/constants/routes'
 import Cities from '@src/pages/Cities'
 import Weather from '@src/pages/Weather'
 import Home from '@src/pages/Home'
+import RootLayout from './pages/RootLayout'
 
 const router = createBrowserRouter([
   {
-    path: routes.HOME,
-    element: <Home />,
-  },
-  {
-    path: routes.CITIES,
-    element: <Cities />,
-  },
-  {
-    path: routes.WEATHER,
-    element: <Weather />,
+    path: routes.ROOT,
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+      {
+        path: routes.CITIES,
+        element: <Cities />,
+      },
+      {
+        path: routes.WEATHER,
+        element: <Weather />,
+      },
+    ],
   },
 ])
 
