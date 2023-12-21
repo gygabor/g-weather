336f3ff99f3e09370cf41ae6fb713bf56ac88c31
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-21 17:16:10 +0100

    fix(Icon button): size

--- src/components/CustomIconButton/styles.ts
+++ src/components/CustomIconButton/styles.ts
@@ -2,12 +2,12 @@ import styled from '@emotion/styled'
 import { IconButton } from '@mui/material'
 
 interface StyledIconButtonProps {
-  size: 'small' | 'medium' | 'large'
+  size: 'small' | 'medium' | 'large' | 'xlarge'
 }
 
 const iconSize = {
-  small: '1rem',
-  medium: '1.5rem',
+  small: '1.5rem',
+  medium: '2rem',
   large: '4rem',
 }
 
