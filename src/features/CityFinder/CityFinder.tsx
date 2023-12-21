ca1a4e57a3a6d5331d9f1917ba0cea3d460a36e4
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-21 17:21:46 +0100

    feat(CityFinder): use custom autocomplete

--- src/features/CityFinder/CityFinder.tsx
+++ src/features/CityFinder/CityFinder.tsx
@@ -1,7 +1,36 @@
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore'
+import { Autocomplete, InputAdornment, TextField } from '@mui/material'
 import { FC } from 'react'
+import {
+  Adornment,
+  AutocompleteIcon,
+} from '../../components/AutocompleteField/styles'
+import AutocompleteField from '@src/components/AutocompleteField'
 
-const SaveCity: FC = () => {
-  return <div></div>
+const options = [
+  'Budapest',
+  'London',
+  'New York',
+  'Berlin',
+  'Paris',
+  'Rome',
+  'Madrid',
+  'Vienna',
+  'Prague',
+  'Amsterdam',
+  'Brussels',
+]
+
+const CityFinder: FC = () => {
+  const onChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    console.log(event.target.value)
+  }
+
+  return (
+    <>
+      <AutocompleteField options={options} onChange={onChange} />
+    </>
+  )
 }
 
-export default SaveCity
+export default CityFinder
