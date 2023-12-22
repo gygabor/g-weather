04282591b1649f1292761f0ffbe8df4d33a23be3
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-22 10:43:25 +0100

    feat(Home): get cities from store

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,20 @@
 import CustomIconButton from '@src/components/CustomIconButton'
-import CityList from '@src/features/CityList'
+import CityList from '@src/components/CityList'
 import { FC } from 'react'
 import { Link } from 'react-router-dom'
+import routes from '@src/constants/routes'
+import { useAppSelector } from '@src/services/store/hooks'
 
 const Home: FC = () => {
+  const cities = useAppSelector((state) => state.cities)
+  const onClick = (id: number) => {
+    console.log(id)
+  }
+
   return (
     <>
-      <CityList />
-      <Link to="/cities">
+      <CityList onClick={onClick} cities={cities} />
+      <Link to={routes.CITIES}>
         <CustomIconButton iconType="add" color="success" size="large" />
       </Link>
     </>
