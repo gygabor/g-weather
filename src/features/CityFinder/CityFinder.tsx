7b11a16146d0651e6dabbcbc1c68c6be29151816
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-22 11:03:45 +0100

    refactor(CityFinder): create parseCountry util

--- src/features/CityFinder/CityFinder.tsx
+++ src/features/CityFinder/CityFinder.tsx
@@ -3,19 +3,12 @@ import AutocompleteField from '@src/components/AutocompleteField'
 import { useFetch } from '@src/hooks'
 import { COUNTRIES_URL } from '@src/constants/links'
 import { City } from '@src/types'
-import { useAppDispatch } from '@src/services/store/hooks'
+import { useAppDispatch } from '@src/services/store'
 import { saveCity } from './citiesSlice'
 import routes from '@src/constants/routes'
 import { useNavigate } from 'react-router-dom'
-
-interface Country {
-  capital: string[]
-  capitalInfo: {
-    latlng: number[]
-  }
-  latlng: number[]
-  population: number
-}
+import { Country } from './types'
+import parseCounties from './parseCounties'
 
 const CityFinder: FC = () => {
   const dispatch = useAppDispatch()
@@ -24,24 +17,7 @@ const CityFinder: FC = () => {
 
   const cities: City[] = useMemo(() => {
     if (data) {
-      return data.flatMap((country, i) => {
-        if (country.capital) {
-          return [
-            {
-              id: `${i}-${country.population}`,
-              name: country.capital[0],
-              lat: country?.capitalInfo?.latlng
-                ? country?.capitalInfo?.latlng[0]
-                : country.latlng[0],
-              lng: country?.capitalInfo?.latlng
-                ? country?.capitalInfo?.latlng[0]
-                : country.latlng[1],
-            },
-          ]
-        } else {
-          return []
-        }
-      })
+      return parseCounties(data)
     } else {
       return []
     }
