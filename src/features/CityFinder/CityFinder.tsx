9563903e7a3ad54d9528a391381ecf66a9101837
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-22 10:41:19 +0100

    feat(CityFinder): fetch data, handle selection

--- src/features/CityFinder/CityFinder.tsx
+++ src/features/CityFinder/CityFinder.tsx
@@ -1,111 +1,60 @@
-import {
-  FC,
-  SyntheticEvent,
-  useCallback,
-  useEffect,
-  useMemo,
-  useState,
-} from 'react'
+import { FC, useMemo } from 'react'
 import AutocompleteField from '@src/components/AutocompleteField'
 import { useFetch } from '@src/hooks'
-import { CITY_URL } from '@src/constants/links'
-import { fetchData } from '@src/services/fetchData/fetchData'
-import debounce from './debounce'
-// import { debounce } from '@mui/material/utils'
+import { COUNTRIES_URL } from '@src/constants/links'
+import { City } from '@src/types'
+import { useAppDispatch } from '@src/services/store/hooks'
+import { saveCity } from './citiesSlice'
+import routes from '@src/constants/routes'
+import { useNavigate } from 'react-router-dom'
 
-// const options = [
-//   'Budapest',
-//   'London',
-//   'New York',
-//   'Berlin',
-//   'Paris',
-//   'Rome',
-//   'Madrid',
-//   'Vienna',
-//   'Prague',
-//   'Amsterdam',
-//   'Brussels',
-//   'Lisbon',
-//   'Stockholm',
-//   'Oslo',
-//   'Copenhagen',
-//   'Helsinki',
-//   'Moscow',
-//   'Warsaw',
-// ]
-
-interface City {
-  name: string
-  latitude: number
-  longitude: number
-  country: string
+interface Country {
+  capital: string[]
+  capitalInfo: {
+    latlng: number[]
+  }
+  latlng: number[]
   population: number
-  is_capital: boolean
 }
 
 const CityFinder: FC = () => {
-  const [value, setValue] = useState<string | null>(null)
-  const [options, setOptions] = useState<City[]>([])
-  const [inputValue, setInputValue] = useState('')
-  const [isLoading, setLoading] = useState(false)
+  const dispatch = useAppDispatch()
+  const navigate = useNavigate()
+  const { data } = useFetch<Country[]>(COUNTRIES_URL)
 
-  // url: `${CITY_URL}${searchValue}`,
-
-  // useEffect(() => {
-  //   console.log({ value, inputValue })
-  // }, [value, inputValue])
-
-  const fetch = useCallback(
-    () =>
-      debounce(async (url: string) => {
-        setLoading(true)
-        try {
-          const result = await fetchData<City[]>(url, true)
-          console.log({ result })
-          const capitals = result.filter((city) => city.is_capital)
-          setOptions(capitals)
-        } catch (err) {
-          console.log(err)
-        } finally {
-          setLoading(false)
+  const cities: City[] = useMemo(() => {
+    if (data) {
+      return data.flatMap((country, i) => {
+        if (country.capital) {
+          return [
+            {
+              id: `${i}-${country.population}`,
+              name: country.capital[0],
+              lat: country?.capitalInfo?.latlng
+                ? country?.capitalInfo?.latlng[0]
+                : country.latlng[0],
+              lng: country?.capitalInfo?.latlng
+                ? country?.capitalInfo?.latlng[0]
+                : country.latlng[1],
+            },
+          ]
+        } else {
+          return []
         }
-      }, 200),
-    [],
-  )
-
-  useEffect(() => {
-    let timerId: NodeJS.Timeout
-
-    const fetchClient = fetch()
-    fetchClient(`${CITY_URL}${inputValue}`)
-
-    // if (inputValue) {
-    //   fetchClient(`${CITY_URL}${inputValue}`)
-    // } else {
-    //   setOptions([])
-    // }
-
-    return () => {
-      clearTimeout(timerId)
+      })
+    } else {
+      return []
     }
-  }, [value, inputValue])
+  }, [data])
 
-  const onChange = (
-    event: SyntheticEvent<Element, Event>,
-    newValue: string | null,
-  ) => {
-    console.log(event)
-    console.log({ newValue })
+  const onClick = (value: City) => {
+    dispatch(saveCity(value))
+    navigate(routes.ROOT)
   }
 
   return (
     <>
-      <AutocompleteField
-        options={options}
-        onChange={onChange}
-        value={value}
-        setInputValue={setInputValue}
-      />
+      <AutocompleteField options={cities} onClick={onClick} />
     </>
   )
 }
