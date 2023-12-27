bbb406989372c4d8ebeb6f1f6c8df507e9d20f26
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-27 14:17:02 +0100

    feat(components): add citylist

---
+++ src/components/CityList/CityList.tsx
@@ -0,0 +1,22 @@
+import { City } from '@src/types'
+import { FC } from 'react'
+import { CityName } from './styles'
+
+interface Props {
+  cities: City[]
+  onClick: (city: City) => void
+}
+
+const CityList: FC<Props> = ({ cities, onClick }) => {
+  return (
+    <>
+      {cities.map((city) => (
+        <CityName variant="h5" key={city.id} onClick={() => onClick(city)}>
+          {city.name}
+        </CityName>
+      ))}
+    </>
+  )
+}
+
+export default CityList
