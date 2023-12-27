6df8810fd1eb2bd804bc85d0ba029cd006c4063e
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-27 14:17:30 +0100

    feat(links): add openweather url

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,1 +1,2 @@
 export const COUNTRIES_URL = 'https://restcountries.com/v3.1/all'
+export const OPEN_WEATHER_URL = `https://api.openweathermap.org/data/3.0/onecall?exclude=minutely,hourly,daily,alerts&units=metric`
