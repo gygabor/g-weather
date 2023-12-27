e91fbf248f2eb52450eacb13968eee3104e188d4
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-27 17:36:10 +0100

    feat(Weather page): add skeleton

--- src/pages/Weather/Weather.tsx
+++ src/pages/Weather/Weather.tsx
@@ -1,35 +1,40 @@
 import { Skeleton } from '@mui/material'
 import { OPEN_WEATHER_URL } from '@src/constants/links'
 import Clock from '@src/features/Clock'
+import WeatherInfo from '@src/features/WeatherInfo'
 import { useFetch } from '@src/hooks'
-import { FC, useMemo } from 'react'
+import { WeatherType } from '@src/types'
+import { FC } from 'react'
 import { useLocation } from 'react-router-dom'
 
 const Weather: FC = () => {
   const { state } = useLocation()
+  const { city } = state
   const apiKey = process.env['OPEN_WEATHER_API_KEY']
-  const url = `${OPEN_WEATHER_URL}&lat=${state.city.lat}&lon=${state.city.lon}&appid=${apiKey}`
+  const url = `${OPEN_WEATHER_URL}&lat=${city.lat}&lon=${city.lon}&appid=${apiKey}`
 
-  const { data, isLoading, error } = useFetch(url)
+  const { data, isLoading, error } = useFetch<WeatherType>(url)
 
   if (error) {
     console.log(error)
   }
 
-  const weather = useMemo(() => {
-    if (data) {
-      return data
-    } else {
-      return {}
-    }
-  }, [data])
-
   return (
     <>
-      {isLoading || !weather.current ? (
-        <Skeleton variant="rectangular" width={210} height={118} />
+      {isLoading || !data ? (
+        <>
+          <Skeleton variant="rounded" width={80} height={68} />
+          <Skeleton variant="rounded" width={352} height={200} />
+        </>
       ) : (
-        <Clock time={weather.current.dt} offset={weather.timezone_offset} />
+        <>
+          <Clock
+            time={data.current.dt}
+            offset={data.timezone_offset}
+            city={city.name}
+          />
+          <WeatherInfo weather={data} />
+        </>
       )}
     </>
   )
