6f941b32a320f331544a288451d117fe5186bdf3
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-28 11:45:45 +0100

    feat: autocomplete render options

---
+++ src/features/CityFinder/renderOptions.tsx
@@ -0,0 +1,40 @@
+import { CityType } from '@src/types'
+import { OptionTypography } from './styles'
+import { Typography } from '@mui/material'
+
+const renderOptions = (
+  props: React.HTMLAttributes<HTMLLIElement>,
+  option: CityType,
+  { inputValue }: { inputValue: string },
+) => {
+  const lowerCaseName = option.name.toLowerCase()
+  const lowerCaseInputValue = inputValue.toLowerCase()
+  const start = lowerCaseName.indexOf(lowerCaseInputValue)
+  if (inputValue === '' || start === -1) {
+    return (
+      <li {...props} key={option.id}>
+        <OptionTypography variant="h6">{option.name}</OptionTypography>
+      </li>
+    )
+  }
+  const end = start + inputValue.length
+  const before = option.name.slice(0, start)
+  const highlighted = option.name.slice(start, end)
+  const after = option.name.slice(end)
+
+  return (
+    <li {...props} key={option.id}>
+      <OptionTypography variant="h6">
+        {before.replace(/ /g, '\u00A0')}
+      </OptionTypography>
+      <Typography variant="h6">
+        {highlighted.replace(/ /g, '\u00A0')}
+      </Typography>
+      <OptionTypography variant="h6">
+        {after.replace(/ /g, '\u00A0')}
+      </OptionTypography>
+    </li>
+  )
+}
+
+export default renderOptions
