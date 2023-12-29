9bfdb535cddadbb17c9d4d3c96172379a367601a
Gyorgy Gabor <gyorgyg@gmail.com>
2023-12-29 10:08:29 +0100

    feat: setup test environment

---
+++ jest.config.ts
@@ -0,0 +1,11 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
+  moduleNameMapper: {
+    '^@src/(.*)$': '<rootDir>/src/$1',
+  },
+}
